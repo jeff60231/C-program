109dddd86c7307b7449b84dacc78f5729a9991ea
jeff60231 <st123456ts@yahoo.com.tw>
2014-03-24 12:52:05 +0800

    Update Pi.cpp

--- Pi.cpp
+++ Pi.cpp
@@ -1,8 +1,20 @@
-// 20140310-2.cpp : 定義主控台應用程式的進入點。
+// 000.cpp : 定義主控台應用程式的進入點。
 //
+
+#include "stdafx.h"
+
+
+/**
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}*/
+
+
 
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
 
@@ -10,101 +22,102 @@ int main(){
 	int i;
 	int times;
 	double denominator;
-	double tempAnswer = 2.0/3 ;
-	double Pi ;
+	double tempAnswer = 2.0 / 3;
+	double Pi;
 
 
-printf("enter how many times you want to calculate");
-scanf("%d" , &times);
+	printf("enter how many times you want to calculate");
+	scanf_s("%d", &times);
 
-for(i = 2 ; i <= times ; i++){
-	if((i % 2) == 0)
-	{
-	denominator = 1 + 4 * (i / 2);
+	for (i = 2; i <= times; i++){
+		if ((i % 2) == 0)
+		{
+			denominator = 1 + 4 * (i / 2);
+		}
+		else
+		{
+			denominator = -3 - 4 * (int)(i / 2);
+		}
+
+		tempAnswer = tempAnswer + (1 / denominator);
+		Pi = 4 * tempAnswer;
+
+		if ((int)(Pi * 100) == 314){
+			printf("When we calculate %d times , we will get 3.14\t\n", i);
+			break;
+		}
 	}
-	else
-	{
-	denominator = -3 - 4 * (int)(i / 2) ; 
-	}
-
-	tempAnswer = tempAnswer + (1 / denominator);
-	Pi = 4 * tempAnswer ;
-	
-	if( (int)(Pi * 100) == 314 ){
-	printf("When we calculate %d times , we will get 3.14\t\n", i);
-	break;
-	}
-}
-	i = 0 ;
+	i = 0;
 	denominator = 0;
-	tempAnswer = 2.0/3 ;
-	Pi  = 0;
-for(i = 2 ; i <= times ; i++){
-	if((i % 2) == 0)
-	{
-	denominator = 1 + 4 * (i / 2);
-	}
-	else
-	{
-	denominator = -3 - 4 * (int)(i / 2) ; 
-	}
+	tempAnswer = 2.0 / 3;
+	Pi = 0;
+	for (i = 2; i <= times; i++){
+		if ((i % 2) == 0)
+		{
+			denominator = 1 + 4 * (i / 2);
+		}
+		else
+		{
+			denominator = -3 - 4 * (int)(i / 2);
+		}
 
-	tempAnswer = tempAnswer + (1 / denominator);
-	Pi = 4 * tempAnswer ;
-	
-	if( (int)(Pi * 1000) == 3141){
-	printf("When we calculate %d times , we will get 3.141\t\n", i);
-	break;
+		tempAnswer = tempAnswer + (1 / denominator);
+		Pi = 4 * tempAnswer;
+
+		if ((int)(Pi * 1000) == 3141){
+			printf("When we calculate %d times , we will get 3.141\t\n", i);
+			break;
+		}
 	}
-}
-	i = 0 ;
+	i = 0;
 	denominator = 0;
-	tempAnswer = 2.0/3 ;
-	Pi  = 0;
-for(i = 2 ; i <= times ; i++){
-	if((i % 2) == 0)
-	{
-	denominator = 1 + 4 * (i / 2);
-	}
-	else
-	{
-	denominator = -3 - 4 * (int)(i / 2) ; 
-	}
+	tempAnswer = 2.0 / 3;
+	Pi = 0;
+	for (i = 2; i <= times; i++){
+		if ((i % 2) == 0)
+		{
+			denominator = 1 + 4 * (i / 2);
+		}
+		else
+		{
+			denominator = -3 - 4 * (int)(i / 2);
+		}
 
-	tempAnswer = tempAnswer + (1 / denominator);
-	Pi = 4 * tempAnswer ;
-	
-	if( (int)(Pi * 10000) == 31415){
-	
-	printf("When we calculate %d times , we will get 3.1415\t\n", i);
-	break;
+		tempAnswer = tempAnswer + (1 / denominator);
+		Pi = 4 * tempAnswer;
+
+		if ((int)(Pi * 10000) == 31415){
+
+			printf("When we calculate %d times , we will get 3.1415\t\n", i);
+			break;
+		}
 	}
-}	
-	i = 0 ;
+	i = 0;
 	denominator = 0;
-	tempAnswer = 2.0/3 ;
-	Pi  = 0;
-for(i = 2 ; i <= times ; i++){
-	if((i % 2) == 0)
-	{
-	denominator = 1 + 4 * (i / 2);
-	}
-	else
-	{
-	denominator = -3 - 4 * (int)(i / 2) ; 
+	tempAnswer = 2.0 / 3;
+	Pi = 0;
+	for (i = 2; i <= times; i++){
+		if ((i % 2) == 0)
+		{
+			denominator = 1 + 4 * (i / 2);
+		}
+		else
+		{
+			denominator = -3 - 4 * (int)(i / 2);
+		}
+
+		tempAnswer = tempAnswer + (1 / denominator);
+		Pi = 4 * tempAnswer;
+
+		if ((int)(Pi * 100000) == 314159){
+
+			printf("When we calculate %d times , we will get 3.14159\t\n", i);
+			break;
+		}
 	}
 
-	tempAnswer = tempAnswer + (1 / denominator);
-	Pi = 4 * tempAnswer ;
-	
-	if( (int)(Pi * 100000) == 314159){
-	
-	printf("When we calculate %d times , we will get 3.14159\t\n", i);
-	break;
-	}
-}
+	system("pause"); 
+	return(0);
 
-	
-	
 }
 
